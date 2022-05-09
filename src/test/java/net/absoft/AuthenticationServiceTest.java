6acc30ab33ca2302c613a923f6eb635e5b510cb2
Dmytro Chepelev <dmytro.chepelev@gmail.com>
2022-05-09 22:53:13 +0300

    Added test testFailAuthentications with DataProvider
    @Test  AuthenticationServiceTest::testFailAuthentications
    @DataProvider AuthenticationServiceTest::dataProviderFailAuthentications
    {empty email, invalid email, empty password, wrong password}

--- src/test/java/net/absoft/AuthenticationServiceTest.java
+++ src/test/java/net/absoft/AuthenticationServiceTest.java
@@ -6,6 +6,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import net.absoft.data.Response;
 import net.absoft.services.AuthenticationService;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
@@ -68,5 +69,28 @@ public class AuthenticationServiceTest {
     final Pattern pattern = Pattern.compile("\\S{32}", Pattern.MULTILINE);
     final Matcher matcher = pattern.matcher(token);
     return matcher.matches();
+  }
+
+  @Test(
+          groups = { "negative" },
+          dataProvider = "failAuthentications"
+  )
+  public void testFailAuthentications(String testName, String email, String password, Integer errorCode, String errorMessage) {
+    Response response = new AuthenticationService().authenticate(email, password);
+    SoftAssert sa = new SoftAssert();
+    sa.assertEquals(response.getCode(), errorCode.intValue(), "Wrong error code:");
+    sa.assertEquals(response.getMessage(), errorMessage,"Wrong error message:");
+    sa.assertAll("Unexpected response by '" + testName + "' test");
+  }
+
+  @DataProvider(name = "failAuthentications")
+  public Object[][] dataProviderFailAuthentications () {
+    return new Object[][]{
+            // String testName, String email, String password, Integer errorCode, String errorMessage
+            {"empty email",     "",               "password1",        400, "Email should not be empty string"},
+            {"invalid email",   "user1",          "password1",        400, "Invalid email"},
+            {"empty password",  "user1@test.com", "",                 400, "Password should not be empty string"},
+            {"wrong password",  "user1@test.com", "wrong_password1",  401, "Invalid email or password"}
+    };
   }
 }
