92f087a63cfa0d9b8ad934373172f983e6e94824
Dmytro Chepelev <dmytro.chepelev@gmail.com>
2022-05-10 02:51:59 +0300

    Created class for demonstrate SetUp method. Relocated "negative" single methods
    AuthenticationServiceTest --> AuthenticationServiceWithSetUpTest
    ::testAuthenticationWithWrongPassword
    ::testAuthenticationWithEmptyEmail
    ::testAuthenticationWithInvalidEmail
    ::testAuthenticationWithEmptyPassword

--- src/test/java/net/absoft/AuthenticationServiceTest.java
+++ src/test/java/net/absoft/AuthenticationServiceTest.java
@@ -1,7 +1,5 @@
 package net.absoft;
 
-import static org.testng.Assert.assertEquals;
-
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import net.absoft.data.Response;
@@ -22,47 +20,6 @@ public class AuthenticationServiceTest {
       softAssert.assertTrue(validateToken(response.getMessage()),
         "Token should be the 32 digits string. Got: " + response.getMessage());
     softAssert.assertAll();
-  }
-
-  @Test(
-          groups = { "negative" }
-  )
-  public void testAuthenticationWithWrongPassword() {
-    Response response = new AuthenticationService()
-        .authenticate("user1@test.com", "wrong_password1");
-    assertEquals(response.getCode(), 401, "Response code should be 401");
-    assertEquals(response.getMessage(), "Invalid email or password",
-        "Response message should be \"Invalid email or password\"");
-  }
-
-  @Test(
-          groups = { "negative" }
-  )
-  public void testAuthenticationWithEmptyEmail() {
-    Response response = new AuthenticationService().authenticate("", "password1");
-    assertEquals(response.getCode(), 400, "Response code should be 400");
-    assertEquals(response.getMessage(), "Email should not be empty string",
-        "Response message should be \"Email should not be empty string\"");
-  }
-
-  @Test(
-          groups = { "negative" }
-  )
-  public void testAuthenticationWithInvalidEmail() {
-    Response response = new AuthenticationService().authenticate("user1", "password1");
-    assertEquals(response.getCode(), 400, "Response code should be 400");
-    assertEquals(response.getMessage(), "Invalid email",
-        "Response message should be \"Invalid email\"");
-  }
-
-  @Test(
-          groups = { "negative" }
-  )
-  public void testAuthenticationWithEmptyPassword() {
-    Response response = new AuthenticationService().authenticate("user1@test.com", "");
-    assertEquals(response.getCode(), 400, "Response code should be 400");
-    assertEquals(response.getMessage(), "Password should not be empty string",
-        "Response message should be \"Password should not be empty string\"");
   }
 
   private boolean validateToken(String token) {
